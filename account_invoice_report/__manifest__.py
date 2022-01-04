8dd53b5604a814abf347ded9b624fdcc1601a622
beshoy <beshoywageh3@gmail.com>
2022-01-04 12:48:03 +0200

    Fixed Inv

--- account_invoice_report/__manifest__.py
+++ account_invoice_report/__manifest__.py
@@ -4,6 +4,7 @@
     'website': "https://www.linkedin.com/in/beshoy-wageh-701ba7116/",
     'depends': ['base', 'l10n_gcc_invoice','l10n_sa_invoice'],
     'data': [
+        'views/views.xml',
         'views/templates.xml',
     ],
 }
